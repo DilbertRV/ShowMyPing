3f618b3eb106112a8f91c674f3f5c563d8128b76
Dilbert Ramirez <gearxdtrece@gmail.com>
2022-01-08 16:09:31 -0600

    Add project files.

---
+++ ShowMyPing/CustomAddressForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace ShowMyPing
+{
+    partial class CustomAddressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomAddressForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.txtCustomAddress = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnConfirm);
+            this.panel1.Controls.Add(this.txtCustomAddress);
+            this.panel1.Controls.Add(this.txtTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 107);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(145, 67);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnConfirm.Location = new System.Drawing.Point(64, 67);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.TabIndex = 2;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // txtCustomAddress
+            // 
+            this.txtCustomAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCustomAddress.Location = new System.Drawing.Point(44, 41);
+            this.txtCustomAddress.Name = "txtCustomAddress";
+            this.txtCustomAddress.Size = new System.Drawing.Size(198, 20);
+            this.txtCustomAddress.TabIndex = 1;
+            this.txtCustomAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.AutoSize = true;
+            this.txtTitle.Location = new System.Drawing.Point(88, 21);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(98, 13);
+            this.txtTitle.TabIndex = 0;
+            this.txtTitle.Text = "Enter Host Address";
+            // 
+            // CustomAddressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 107);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(297, 146);
+            this.Name = "CustomAddressForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enter Custom Host Address";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.TextBox txtCustomAddress;
+        private System.Windows.Forms.Label txtTitle;
+    }
+}
---
+++ ShowMyPing/Program.cs
@@ -0,0 +1,237 @@
+using Microsoft.Win32;
+using ShowMyPing.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace ShowMyPing
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            CultureInfo.DefaultThreadCurrentUICulture = System.Threading.Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ShowMyPingApplicationContext());
+        }
+    }
+
+    public class ShowMyPingApplicationContext : ApplicationContext
+    {
+        CustomAddressForm form = new CustomAddressForm();
+
+        private string ping = UserSettings.Default.pingUser;
+        private string averagePing;
+        List<string> pingData = new List<string>();
+        List<int> pingDataInt = new List<int>();
+
+        private ToolStripMenuItem defaultAddress;
+        private ToolStripMenuItem customAddress;
+        private ToolStripMenuItem startupMenu;
+        private NotifyIcon notifyIcon;
+        private int current = 0;
+
+        private Timer pingTimer = new Timer();
+
+        public ShowMyPingApplicationContext()
+        {
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+
+            defaultAddress = new ToolStripMenuItem("Default Address", null, SetDefault);
+            if (IsDefaultEnabled())
+            {
+                defaultAddress.Checked = true;
+            }
+
+            customAddress = new ToolStripMenuItem("Custom Address", null, OpenCustomAddressForm);
+
+            startupMenu = new ToolStripMenuItem("Startup", null, SetStartup);
+            if (IsStartupEnabled())
+            {
+                startupMenu.Checked = true;
+            }
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip(new Container());
+            contextMenuStrip.Items.AddRange(new ToolStripItem[]
+            {
+                defaultAddress,
+                customAddress,
+                startupMenu,
+                new ToolStripMenuItem("Exit", null, Exit)
+            });
+
+            notifyIcon = new NotifyIcon()
+            {
+                Icon = Resources.ico_ping_light,
+                ContextMenuStrip = contextMenuStrip,
+                Text = "Show My Ping",
+                Visible = true
+            };
+
+            VerifyPingHost();
+            StartPingTest();
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            UserSettings.Default.pingUser = ping;
+            UserSettings.Default.Save();
+        }
+        public void VerifyPingHost()
+        {
+            if (IsDefaultEnabled())
+                defaultAddress.Checked = true;
+            else
+                customAddress.Checked = true;
+            notifyIcon.Text = "Show My Ping" + "\n" + ping;
+        }
+
+        private bool IsDefaultEnabled()
+        {
+            return ping == "8.8.8.8" ? true : false;
+        }
+
+        private void SetDefault(object sender, EventArgs e)
+        {
+            if (!IsDefaultEnabled())
+               defaultAddress.Checked = !defaultAddress.Checked;
+            if (defaultAddress.Checked)
+            {
+                ping = "8.8.8.8";
+                customAddress.Checked = false;
+            }
+            notifyIcon.Text = "Show My Ping" + "\n" + ping;
+        }
+
+        private bool IsStartupEnabled()
+        {
+            string keyName = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            using (RegistryKey rKey = Registry.CurrentUser.OpenSubKey(keyName))
+            {
+                return (rKey.GetValue(Application.ProductName) != null) ? true : false;
+            }
+        }
+
+        private void SetStartup(object sender, EventArgs e)
+        {
+            startupMenu.Checked = !startupMenu.Checked;
+            string keyName = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            using (RegistryKey rKey = Registry.CurrentUser.OpenSubKey(keyName, true))
+            {
+                if (startupMenu.Checked)
+                {
+                    rKey.SetValue(Application.ProductName, Process.GetCurrentProcess().MainModule.FileName);
+                }
+                else
+                {
+                    rKey.DeleteValue(Application.ProductName, false);
+                }
+                rKey.Close();
+            }
+        }
+
+        private void OpenCustomAddressForm(object sender, EventArgs e)
+        {
+            defaultAddress.Checked = false;
+            form.ShowDialog();
+            if (form.customAddress != null)
+            {
+                ping = form.customAddress;
+                customAddress.Checked = true;
+            }
+            else
+                defaultAddress.Checked = true;
+            notifyIcon.Text = "Show My Ping" + "\n" + ping;
+        }
+
+            private void Exit(object sender, EventArgs e)
+        {
+            pingTimer.Stop();
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void ShowLatency(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                StartObservePing();
+        }
+        private void StartPingTest()
+        {
+            notifyIcon.MouseClick += ShowLatency;
+        }
+
+        public void PingTimeAverage()
+        {
+            try
+            {
+                Ping pingClass = new Ping();
+
+                PingReply pingReply = pingClass.Send(ping);
+                pingData.Add(pingReply.RoundtripTime.ToString() + "ms");
+                pingDataInt.Add(((int)pingReply.RoundtripTime));
+
+                current++;
+
+                for (int i = 0; i < pingData.Count; i++)
+                    notifyIcon.Text = (pingData[i]);
+
+                if (current == 4)
+                {
+                    pingTimer.Stop();
+                    pingTimer.Tick -= ObservePingTick;
+                    current = 0;
+
+                    CalculatingAverage();
+                    string Data = string.Join(" - ", pingData);
+
+                    notifyIcon.BalloonTipTitle = "Pinging " + ping;
+                    notifyIcon.BalloonTipText = "Ping: " + Data + "\n" + "Average: " + averagePing;
+
+                    notifyIcon.Text = "Show My Ping" + "\n" + ping;
+
+                    notifyIcon.ShowBalloonTip(5000);
+                    pingData.Clear();
+                    pingDataInt.Clear();
+                }
+            }
+            catch (PingException e)
+            {
+                pingTimer.Stop();
+                pingTimer.Tick -= ObservePingTick;
+                current = 0;
+                MessageBox.Show(e.Message + "\nCheck the address and enter a valid one." + "\nAddress: " + ping, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        public void CalculatingAverage()
+        {
+            int sum = 0;
+
+            foreach (int ping in pingDataInt)
+            {
+                sum += ping;
+            }
+            averagePing = (sum / 4).ToString() + "ms";
+        }
+
+        private void ObservePingTick(object sender, EventArgs e)
+        {
+            PingTimeAverage();
+        }
+
+        private void StartObservePing() 
+        {
+            pingTimer.Interval = 1100;
+            pingTimer.Tick += new EventHandler(ObservePingTick);
+            pingTimer.Start();
+        }
+    }
+}
